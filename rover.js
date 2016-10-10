b2d09623591e8dd83c9b6434a94084ff33b8940c
Maxime Mercy <mxmercy@gmail.com>
2016-10-10 13:18:42 +0200

    init + function right and left

---
+++ rover.js
@@ -0,0 +1,68 @@
+var myRover = {
+  position: [0,0], 
+  direction: 'N'
+};
+
+
+function goForward(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]++
+      break;
+    case 'E':
+      rover.position[1]++
+      break;
+    case 'S':
+      rover.position[0]--
+      break;
+    case 'W':
+      rover.position[1]--
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+
+function roverLeft(rover) { 
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W'
+      break;
+    case 'W':
+      rover.direction = 'S'
+      break;
+    case 'S':
+      rover.direction = 'E'
+      break;
+    case 'E':
+      rover.direction = 'N'
+      break;
+  };
+  console.log("New Rover Direction: Facing " + rover.direction )
+}
+
+function roverRight(rover) { 
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E'
+      break;
+    case 'E':
+      rover.direction = 'S'
+      break;
+    case 'S':
+      rover.direction = 'W'
+      break;
+    case 'W':
+      rover.direction = 'N'
+      break;
+  };
+  console.log("New Rover Direction: Facing " + rover.direction )
+}
+
+goForward(myRover);
+roverLeft(myRover);
+roverLeft(myRover);
+roverRight(myRover);
+roverLeft(myRover);
+
